b339a20148b55a16461a954917296d073cfe0db2
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-06 19:13:57 +0530

    .

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 // Promises
-const promise = new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("The promise has been resolved");
   }, 1000);
@@ -8,21 +8,49 @@ const promise = new Promise((resolve, reject) => {
 });
 
 // Promise chaining
-const promise1 = new Promise((resolve, reject) => {
+const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => resolve(1), 1000);
 })
   .then((result) => {
-    console.log(result);
     return result * 2;
   })
   .then((result) => {
-    console.log(result);
     return result * 2;
   })
   .then((result) => {
-    console.log(result);
     return result * 2;
   })
   .then((result) => {
-    console.log(result);
+    console.log(`The final value is ${result}`);
+  });
+
+// The following is not chaining
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+});
+
+promise3.then((result) => {
+  return result * 2;
+});
+
+promise3.then((result) => {
+  return result * 2;
+});
+
+// Returning promises
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+})
+  .then((result) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  })
+  .then((result) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  })
+  .then((result) => {
+    console.log(`The final value is ${result}`);
   });
