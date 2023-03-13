72dfd0cf91260ba62b74bcb48ac23a3852592412
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-13 22:50:08 +0530

    day 19 project added

--- src/components/Day 19 - Countdown Timer/Timer.js
+++ src/components/Day 19 - Countdown Timer/Timer.js
@@ -1,53 +1,26 @@
-import { useState, useRef, useReducer } from "react";
-
-function reducer(time, action) {
-  switch (action.type) {
-    case "increment-seconds": {
-      const nextTime = {
-        ...time,
-        seconds: time.seconds + 1,
-      };
-      return nextTime;
-    }
-
-    case "increment-minutes": {
-      const nextTime = {
-        ...time,
-        seconds: 0,
-        minutes: time.minutes + 1,
-      };
-      return nextTime;
-    }
-
-    case "reset-timer": {
-      const nextTime = {
-        ...time,
-        seconds: 0,
-        minutes: 0,
-      };
-      return nextTime;
-    }
-
-    default: {
-      throw Error("Unknown action: " + action.type);
-    }
-  }
-}
+import { useState, useRef } from "react";
 
 const Timer = () => {
-  const [time, dispatch] = useReducer(reducer, { minutes: 0, seconds: 0 });
+  const [time, setTime] = useState({ minutes: 0, seconds: 0 });
 
   const interval = useRef(null);
 
   const handleTimerStart = () => {
     interval.current = setInterval(() => {
-      console.log(time.seconds);
-      if (time.seconds === 10) {
-        dispatch({ type: "increment-minutes" });
-      } else {
-        console.log("Seconds incremented");
-        dispatch({ type: "increment-seconds" });
-      }
+      setTime((prevTime) => {
+        if (prevTime.seconds === 59) {
+          return {
+            ...prevTime,
+            seconds: 0,
+            minutes: prevTime.minutes + 1,
+          };
+        }
+
+        return {
+          ...prevTime,
+          seconds: prevTime.seconds + 1,
+        };
+      });
     }, 1000);
   };
 
@@ -58,7 +31,9 @@ const Timer = () => {
   const handleTimerReset = () => {
     clearInterval(interval.current);
 
-    dispatch({ type: "reset-timer" });
+    setTime((prevTime) => {
+      return { ...prevTime, seconds: 0, minutes: 0 };
+    });
   };
 
   return (
