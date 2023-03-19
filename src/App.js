0af444da0a8880c3f9246f38d16f319a306ffb79
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-19 22:21:40 +0530

    day 23 project added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import ContactsApp from "./components/Day 25 - Contacts List/ContactsApp";
+import TodoList from "./components/Day 23 - Todo List (Using Reducer & Context)/TodoList";
 
 function App() {
   return (
     <div className="app">
-      <ContactsApp />
+      <TodoList />
     </div>
   );
 }
