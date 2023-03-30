53f7fcf8dcfbed12a8d0be8bb40f4f4c34cb2b3e
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-30 11:44:17 +0530

    code refactored

---
+++ src/components/Day 30 - Weather App/WeatherApp.js
@@ -0,0 +1,13 @@
+const WeatherApp = () => {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <form className="form">
+        <input type="text" placeholder="Search" />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default WeatherApp;
